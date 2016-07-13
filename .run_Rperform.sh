132f53a19e139a498c904a3df0cc3c9cffa09b2c
da-ta-vinci21 <akashtndn.acm@gmail.com>
2016-07-13 15:58:15 +0530

    Another try

--- .run_Rperform.sh
+++ .run_Rperform.sh
@@ -37,3 +37,36 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   echo -e "Done magic with Rperform\n"
 fi
+
+# if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#   echo -e "Starting to update gh-pages for the PR\n"
+#   # echo -e "$ORIGIN_BRANCH"
+#
+#   #go to home and setup git
+#   # cd $HOME
+#   # git config --global user.email "travis@travis-ci.org"
+#   # git config --global user.name "Travis"
+#
+#   #using token clone gh-pages branch
+#   # git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/da-ta-vinci21/Rperform_ggplotly.git  gh-pages > /dev/null
+#   # git clone --quiet --branch=master https://${GH_TOKEN}@github.com/da-ta-vinci21/Rperform_ggplotly.git  master > /dev/null
+#   # git clone --quiet --branch=$ORIGIN_BRANCH https://${GH_TOKEN}@github.com/da-ta-vinci21/Rperform_ggplotly.git  origin > /dev/null
+#
+#   #Run the Rperform functions
+#   touch temp_Rperform.R
+#   echo "Rperform::plot_metrics(\"./tests/unattached.r\", metric = \"time\")" >> temp_Rperform.R
+#   Rscript temp_Rperform.R
+#   rm temp_Rperform.R
+#
+#   git checkout -- *
+#
+#   # cd ./gh-pages
+#   # cp -Rf $HOME/index.html index.html
+#   #
+#   # #add, commit and push files
+#   # git add -f .
+#   # git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+#   # git push -fq origin gh-pages > /dev/null
+#
+#   echo -e "Done magic with Rperform and PR\n"
+# fi
