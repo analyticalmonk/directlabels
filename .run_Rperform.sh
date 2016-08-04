75f75789ab992894fe5b3d3eb51a7c16bdd5dbd2
analyticalmonk <akashtndn.acm@gmail.com>
2016-08-04 12:08:59 +0530

    Testing env vars

--- .run_Rperform.sh
+++ .run_Rperform.sh
@@ -7,15 +7,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   # Run the Rperform functions
   touch temp_Rperform.R
-  echo "Rperform::plot_webpage(test_directory = \"./tests/\", metric = \"time\", output=\"testing\")" >> temp_Rperform.R
+  echo "Rperform::plot_metrics(\"./tests/unattached.R\", metric = \"time\")" >> temp_Rperform.R
   Rscript temp_Rperform.R
   rm temp_Rperform.R
-  rm testing.Rmd
 
   # We copy the generated html file to one level above the current directory (repo) in order
   # to easily move it to the gh-pages directory (which we will download later)
-  mv -Rf testing.html ../index.html
-  # Store the original location (repo to be tested) and go up one level
+  # mv -Rf testing.html ../index.html
+  # # Store the original location (repo to be tested) and go up one level
   pushd ./
   cd ..
 
@@ -23,18 +22,18 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}  gh-pages > /dev/null
 
   # Copy the generated html file to the gh-pages branch and preserve the existing files
-  cd ./gh-pages
-  if [! -f index.html]
-  then
-    mv -Rf index.html index_old.html
-  fi
-  cp -Rf ../index.html index_buildnum${TRAVIS_BUILD_NUMBER}.html
-  cp index_buildnum${TRAVIS_BUILD_NUMBER}.html index.html
+  # cd ./gh-pages
+  # if [! -f index.html]
+  # then
+  #   mv -Rf index.html index_old.html
+  # fi
+  # cp -Rf ../index.html index_buildnum${TRAVIS_BUILD_NUMBER}.html
+  # cp index_buildnum${TRAVIS_BUILD_NUMBER}.html index.html
 
   # Add, commit and push files to the gh-pages branch
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  # git add -f .
+  # git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  # git push -fq origin gh-pages > /dev/null
 
   popd
   echo -e "Done magic with Rperform\n"
